ff2f3fe2c55e51367372fa7b4fa1e7f9f915f7b2
Casey McLaughlin <caseyamcl@gmail.com>
2019-09-09 15:17:03 -0400

    UUID Format and Validator tests.

---
+++ tests/Format/UuidFormatTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Paramee Library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/caseyamcl/paramee
+ * @package caseyamcl/paramee
+ * @author Casey McLaughlin <caseyamcl@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE.md
+ *  file that was distributed with this source code.
+ *
+ *  ------------------------------------------------------------------
+ */
+
+namespace Paramee\Format;
+
+use Paramee\AbstractParamFormatTest;
+use Paramee\Contract\ParamFormatInterface;
+use Paramee\Exception\InvalidParameterException;
+use Paramee\Model\Parameter;
+use Paramee\Type\StringParameter;
+
+class UuidFormatTest extends AbstractParamFormatTest
+{
+    /**
+     * @dataProvider validUuidDataProvider
+     * @param string $value
+     */
+    public function testValidData(string $value): void
+    {
+        $this->assertSame($value, $this->getParameterWithFormat()->prepare($value));
+    }
+
+    public function validUuidDataProvider(): array
+    {
+        return [
+            ['726a1f97-154d-423d-9f5e-06ad9f4b8aed'],
+            ['2b6afd54-24d4-408d-9c73-28126007add4'],
+            ['d378495b-da78-44c8-b389-fe5bce22b9cc']
+        ];
+    }
+
+    /**
+     * @dataProvider invalidUuidDataProvider
+     * @param string $value
+     */
+    public function testInvalidDataThrowsException(string $value): void
+    {
+        $this->expectException(InvalidParameterException::class);
+        $this->getParameterWithFormat()->prepare($value);
+    }
+
+    public function invalidUuidDataProvider(): array
+    {
+        return [
+            ['abc'],
+            ['def'],
+            ['123']
+        ];
+    }
+
+    /**
+     * @return ParamFormatInterface
+     */
+    protected function getFormat(): ParamFormatInterface
+    {
+        return new UuidFormat();
+    }
+
+    /**
+     * @return Parameter
+     */
+    protected function getParameterWithFormat(): Parameter
+    {
+        return (new StringParameter())->setFormat(new UuidFormat());
+    }
+}
---
+++ tests/Paramee/Validation/Rules/ValidUuidTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Paramee Library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/caseyamcl/paramee
+ * @package caseyamcl/paramee
+ * @author Casey McLaughlin <caseyamcl@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE.md
+ *  file that was distributed with this source code.
+ *
+ *  ------------------------------------------------------------------
+ */
+
+namespace Paramee\Validation\Rules;
+
+use Paramee\Validation\Exceptions\ValidUuidException;
+use PHPUnit\Framework\TestCase;
+use Respect\Validation\Exceptions\ComponentException;
+use Respect\Validation\Validator;
+
+class ValidUuidTest extends TestCase
+{
+    /**
+     * @dataProvider validUuidDataProvider
+     */
+    public function testValidateSucceedsWithValidValue(string $uuid): void
+    {
+        $this->assertTrue((new ValidUuid())->assert($uuid));
+    }
+
+    public function validUuidDataProvider(): array
+    {
+        return [
+            ['726a1f97-154d-423d-9f5e-06ad9f4b8aed'],
+            ['2b6afd54-24d4-408d-9c73-28126007add4'],
+            ['d378495b-da78-44c8-b389-fe5bce22b9cc']
+        ];
+    }
+
+    /**
+     * @dataProvider invalidUuidDataProvider
+     * @throws ComponentException
+     */
+    public function testValidateFailsWithInvalidValue(string $uuid): void
+    {
+        $this->expectException(ValidUuidException::class);
+        Validator::buildRule(new ValidUuid())->assert($uuid);
+    }
+
+    public function invalidUuidDataProvider(): array
+    {
+        return [
+            ['abc'],
+            ['def'],
+            ['123']
+        ];
+    }
+}
