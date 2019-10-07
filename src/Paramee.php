e0eeb9106df391ca3ac0d8aad530fe7866353385
Casey McLaughlin <caseyamcl@gmail.com>
2019-10-07 14:13:12 -0400

    More tests...

--- src/Paramee.php
+++ src/Paramee.php
@@ -17,8 +17,11 @@ declare(strict_types=1);
 
 namespace Paramee;
 
+use Paramee\Contract\ParameterDeserializer;
 use Paramee\Model\ParameterList;
-use Paramee\Model\ParameterValuesContext;
+use Paramee\ParamContext\ParamBodyContext;
+use Paramee\ParamContext\ParamHeaderContext;
+use Paramee\ParamContext\ParamPathContext;
 use Paramee\ParamContext\ParamQueryContext;
 use Psr\Log\LoggerInterface;
 
@@ -39,12 +42,30 @@ class Paramee
     }
 
     /**
-     * Paramee constructor.
-     *
-     * @param ParameterValuesContext|null $context
+     * @param LoggerInterface|null $logger
+     * @param ParameterDeserializer|null $deserializer
+     * @return ParameterList
      */
-    public function __construct(ParameterValuesContext $context = null)
+    public static function bodyParams(?LoggerInterface $logger = null, ?ParameterDeserializer $deserializer = null): ParameterList
     {
+        return new ParameterList('body', [], new ParamBodyContext($deserializer, $logger));
+    }
 
+    /**
+     * @param LoggerInterface|null $logger
+     * @return ParameterList
+     */
+    public static function pathParams(?LoggerInterface $logger = null): ParameterList
+    {
+        return new ParameterList('path', [], new ParamPathContext($logger));
+    }
+
+    /**
+     * @param LoggerInterface|null $logger
+     * @return ParameterList
+     */
+    public static function headerParams(?LoggerInterface $logger = null): ParameterList
+    {
+        return new ParameterList('header', [], new ParamHeaderContext($logger));
     }
 }
---
+++ tests/ParameeTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Paramee Library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/caseyamcl/paramee
+ * @package caseyamcl/paramee
+ * @author Casey McLaughlin <caseyamcl@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE.md
+ *  file that was distributed with this source code.
+ *
+ *  ------------------------------------------------------------------
+ */
+
+namespace Paramee;
+
+use Paramee\Model\ParameterList;
+use Paramee\Model\ParameterValuesContext;
+use Paramee\ParamContext\ParamBodyContext;
+use Paramee\ParamContext\ParamHeaderContext;
+use Paramee\ParamContext\ParamPathContext;
+use Paramee\ParamContext\ParamQueryContext;
+use PHPUnit\Framework\TestCase;
+
+class ParameeTest extends TestCase
+{
+    /**
+     * @dataProvider dataValuesProvider
+     * @param ParameterList $params
+     * @param string $expectedClass
+     */
+    public function testFactoryMethods(ParameterList $params, string $expectedClass)
+    {
+        $this->assertInstanceOf(ParameterValuesContext::class, $params->getContext());
+        $this->assertSame($expectedClass, get_class($params->getContext()));
+    }
+
+
+    public function dataValuesProvider()
+    {
+        return [
+            [Paramee::queryParams(), ParamQueryContext::class],
+            [Paramee::headerParams(), ParamHeaderContext::class],
+            [Paramee::bodyParams(), ParamBodyContext::class],
+            [Paramee::pathParams(), ParamPathContext::class]
+        ];
+    }
+}
