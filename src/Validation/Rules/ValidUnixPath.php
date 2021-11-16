f68830517ddbbe4a179cef37f62b026bee0884e3
Casey McLaughlin <caseyamcl@gmail.com>
2021-11-16 12:28:07 -0500

    Ensure `declare(strict_types=1)` on all production files.

--- src/Validation/Rules/ValidUnixPath.php
+++ src/Validation/Rules/ValidUnixPath.php
@@ -14,6 +14,8 @@
  *  ------------------------------------------------------------------
  */
 
+declare(strict_types=1);
+
 namespace OpenApiParams\Validation\Rules;
 
 use Respect\Validation\Rules\AbstractRule;
