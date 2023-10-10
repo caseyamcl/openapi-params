f563880dce06f3648120020e43ee8d094a99058e
Casey McLaughlin <caseyamcl@gmail.com>
2023-10-10 14:41:57 -0400

    Fix file template in the classes

--- src/Validator/ValidObjectExtraProperties.php
+++ src/Validator/ValidObjectExtraProperties.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * OpenApi-Params Library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/caseyamcl/openapi-params
+ *
+ * @author Casey McLaughlin <caseyamcl@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE.md
+ *  file that was distributed with this source code.
+ *
+ *  ------------------------------------------------------------------
+ */
+
+declare(strict_types=1);
+
 namespace OpenApiParams\Validator;
 
 use Attribute;
